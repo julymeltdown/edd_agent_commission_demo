69a00be3d401160d814b12d9704a4ad93770ae52
lhs <quintuplets2000@gmail.com>
2024-11-04 01:34:01 +0900

    chore: sqlc 실행 및 쿼리 추가

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module edd_agent_commission
+
+go 1.23.2
+
+require github.com/kelseyhightower/envconfig v1.4.0
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,19 @@
 package config
+
+import (
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	KafkaBrokers   string `envconfig:"KAFKA_BROKERS" required:"true"`
+	KafkaTopic     string `envconfig:"KAFKA_TOPIC" required:"true"`
+	PostgresqlDSN  string `envconfig:"POSTGRESQL_DSN" required:"true"`
+	HTTPServerPort string `envconfig:"HTTP_SERVER_PORT" default:"8080"`
+}
+
+func LoadConfig() (*Config, error) {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	return &cfg, err
+
+}
